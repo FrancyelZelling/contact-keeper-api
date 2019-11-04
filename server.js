0f1ad469d00f247ceb4e33ffa2255304a241b533
FrancyelZelling <zellingf@gmail.com>
2019-11-04 08:15:24 -0300

    MongoDB Atlas setup completed

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the Contact Keeper API" })
@@ -12,5 +17,7 @@ app.get("/", (req, res) =>
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
